b1f0976c94bc60d8d2c64fb09b618e4cbb53f9c0
Julia <julia.r.lindgren@gmail.com>
2024-10-06 17:54:07 +0100

    Add comment toggle

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -1,36 +1,47 @@
 import PostListItem from './PostListItem.jsx'
-import { useContext, useState, useEffect} from 'react'
+import { useContext, useState, useEffect } from 'react'
 import { PostContext, UserContext } from '../App'
 import CommentListItem from './CommentListItem.jsx';
 import "../styles/CommentList.css";
 
-function CommentList({postId}) {
+function CommentList({ postId }) {
     const { posts } = useContext(PostContext);
     const [comments, setComments] = useState([]);
-    const commentsUrl = `https://boolean-uk-api-server.fly.dev/Julia-Lindgren/post/${postId}/comment`; 
+    const [isExpanded, setIsExpanded] = useState(false);
+    const commentsUrl = `https://boolean-uk-api-server.fly.dev/Julia-Lindgren/post/${postId}/comment`;
 
     const fetchComments = async () => {
         fetch(commentsUrl)
-          .then((response) => response.json())
-          .then((comments) => {
-            setComments(comments);
-          })
-          .catch((error) => {
-            console.error('Error fetching comments:', error);
-          });
-      }
+            .then((response) => response.json())
+            .then((comments) => {
+                setComments(comments);
+            })
+            .catch((error) => {
+                console.error('Error fetching comments:', error);
+            });
+    }
 
-      useEffect(() => {
+    useEffect(() => {
         fetchComments();
     }, [postId, posts]);
 
+    const toggleExpand = () => {
+        setIsExpanded((prev) => !prev);
+    };
+
     return (
-        <div className='commentList'> 
+        <div className='commentList'>
+            {comments.length > 3 && (
+                <span className="toggle-comments" onClick={toggleExpand}>
+                    {isExpanded ? 'See less comments' : 'See previous comments'}
+                </span>
+            )}
             <ul className="comments">
-                {comments.map((comment) => (
+                {comments.slice(0, isExpanded ? comments.length : 3).map((comment) => (
                     <CommentListItem key={comment.id} comment={comment} />
                 ))}
             </ul>
+            
         </div>
     )
 }
